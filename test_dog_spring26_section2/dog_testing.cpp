fe5e4a54271c13099fb5f555d996d550a0e363bc
Jacob Zambardino <Zamsmurjam@gmail.com>
2026-01-20 11:49:54 -0600

    I re-read the assignment instructions and figured I needed to have the user input set the variables for the dog object. I also changed the name of the dog object to a generic name "dog1"

--- test_dog_spring26_section2/dog_testing.cpp
+++ test_dog_spring26_section2/dog_testing.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -54,18 +55,27 @@ public:
 
 int main()
 {
-	Dog casper;
+	Dog dog1;
 	string name;
+	int age;
 
-	cout << "Here is a newly created Dog object" << endl;
-	cout << "Dog name: " << casper.getName() << endl;
-	cout << "Dog age: " << casper.getAge() << endl;
-	cout << "Dog breed: " << casper.getBreed() << endl;
-
-	cout << "Enter a new name for the dog. ";
+	cout << "What is your dog's name? ";
 	cin >> name;
-	casper.setName(name);
+	dog1.setName(name);
 
-	cout << casper.getName() << " goes "; casper.bark();
+	cout << "How old is " << name << "? ";
+	cin >> age;
+	dog1.setAge(age);
+	cin.ignore();
+
+	cout << "What breed is " << name << "? ";
+	getline(cin, name);
+	dog1.setBreed(name);
+
+	cout << endl << "Name: " << "\t" << dog1.getName() << endl;
+	cout << "Breed: " << "\t" << dog1.getBreed() << endl;
+	cout << "Age: " << "\t" << dog1.getAge() << endl << endl;
+
+	cout << dog1.getName() << " goes "; dog1.bark();
 
 }
